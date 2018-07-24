aa12abda5a2a7e115c161892f179024426034d95
gangsijay <8gangsi@163.com>
2018-07-25 00:34:35 +0800

    two

---
+++ SC_TWO-1/oauth-server/src/main/java/com/ou/oauthserver/config/WebMvcConfg.java
@@ -0,0 +1,14 @@
+package com.ou.oauthserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfg implements WebMvcConfigurer {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
+}
